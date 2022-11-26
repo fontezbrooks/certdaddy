2aed87bd3af5879410477b8d0641cbe0e4a96fb4
fontez.brooks <fontez.brooks@cinemassive.com>
2022-11-25 22:58:43 -0500

    building ui

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Home from "./Home";
+import '../styles/Main.css';
 
 export default function App() {
     return(
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ let mainWindow;
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 960,
+        height: 846,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
